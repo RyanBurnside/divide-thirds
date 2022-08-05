4784251809f47cb4907411c211ec8c53f1206656
Ryan Burnside <pixeloutlaw@gmail.com>
2022-08-04 21:40:15 -0600

    Update divide-thirds.scm

--- divide-thirds.scm
+++ divide-thirds.scm
@@ -14,11 +14,11 @@
 
 ;; Register the main function
 (script-fu-menu-register "script-fu-divide-thirds"
-                         "<Image>/Guides/")
+                         "<Image>/Script-Fu/")
 
 ;; Main function
 (define (script-fu-divide-thirds the-image the-drawable)
-  (script-fu-guide-new-percent 1 1 0 66.666)
-  (script-fu-guide-new-percent 1 1 0 33.333)
-  (script-fu-guide-new-percent 1 1 1 66.666)
-  (script-fu-guide-new-percent 1 1 1 33.333))
+  (script-fu-guide-new-percent the-image the-drawable 0 66.666)
+  (script-fu-guide-new-percent the-image the-drawable 0 33.333)
+  (script-fu-guide-new-percent the-image the-drawable 1 66.666)
+  (script-fu-guide-new-percent the-image the-drawable 1 33.333))
