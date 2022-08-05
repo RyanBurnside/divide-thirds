a0f442b6c8dea9c28be6b25c8e863a47ced3c6cb
Ryan Burnside <pixeloutlaw@gmail.com>
2022-08-04 23:06:37 -0600

    Update divide-thirds.scm

--- divide-thirds.scm
+++ divide-thirds.scm
@@ -1,4 +1,39 @@
-;; Yes, I know Python exists but how rare and cool it is to work in Scheme!
+(script-fu-register
+ "script-fu-divide-grid"                         ;func name
+ "Divide into rows and columns using guides."                ;menu label
+ "Divide the canvas into rows and columns by adding guides." ;Description
+ "Ryan Burnside"                                   ;author
+ "No copyright protection"                         ;copyright
+ "August 4 2022"                                   ;date
+ ""                                                ;image type
+
+ ;; Widgets and top level parameters
+ SF-IMAGE       "Image"          0
+ SF-DRAWABLE    "Drawable"       0
+ SF-ADJUSTMENT  "Horizontal Divisions" '(3 1 300 1 4 0 SF-SLIDER)
+ SF-ADJUSTMENT  "Vertical  Divisions" '(3 1 300 1 4 0 SF-SLIDER))
+
+
+;; Register the main function
+(script-fu-menu-register "script-fu-divide-grid"
+                         "<Image>/Script-Fu/")
+
+;; Helper function
+(define (script-fu-divide-sections the-image the-drawable divisions horiz) ;0 for horiz 1 for vert
+  (let loop ((i 0))
+    (when (< i divisions)
+      (script-fu-guide-new-percent the-image the-drawable horiz (* (/ i divisions) 100))
+      (loop (+ i 1)))))
+
+
+;; Main function
+(define (script-fu-divide-grid the-image the-drawable horizontal-divisions vertical-divisions)
+  (let ((horiz 0)
+        (vert 1))
+    (script-fu-divide-sections the-image the-drawable horizontal-divisions horiz)
+    (script-fu-divide-sections the-image the-drawable vertical-divisions vert)))
+
+
 
 (script-fu-register
  "script-fu-divide-thirds"                         ;func name
@@ -20,10 +55,12 @@
 
 ;; Main function
 (define (script-fu-divide-thirds the-image the-drawable)
-  (script-fu-guide-new-percent the-image the-drawable 0 66.666)
-  (script-fu-guide-new-percent the-image the-drawable 0 33.333)
-  (script-fu-guide-new-percent the-image the-drawable 1 66.666)
-  (script-fu-guide-new-percent the-image the-drawable 1 33.333))
+  (let ((horiz 0)
+        (vert 1))
+    (script-fu-guide-new-percent the-image the-drawable horiz 66.666)
+    (script-fu-guide-new-percent the-image the-drawable horiz 33.333)
+    (script-fu-guide-new-percent the-image the-drawable vert 66.666)
+    (script-fu-guide-new-percent the-image the-drawable vert 33.333)))
 
 
 (script-fu-register
@@ -46,13 +83,12 @@
 
 ;; Main function
 (define (script-fu-frame-guides the-image the-drawable)
-  (script-fu-guide-new-percent the-image the-drawable 0 100)
-  (script-fu-guide-new-percent the-image the-drawable 0 0)
-  (script-fu-guide-new-percent the-image the-drawable 1 100)
-  (script-fu-guide-new-percent the-image the-drawable 1 0))
+  (let ((horiz 0)
+        (vert 1))
+    (script-fu-guide-new-percent the-image the-drawable horiz 100)
+    (script-fu-guide-new-percent the-image the-drawable horiz 0)
+    (script-fu-guide-new-percent the-image the-drawable vert 100)
+    (script-fu-guide-new-percent the-image the-drawable vert 0)))
 
-
-;; TODO interface to provide for <divisions across> and <divisions down>
-;; Easy peason lemon squeezy. (But later, it's late tonight)
 
 
