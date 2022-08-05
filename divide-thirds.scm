32d0ac49cd04adb1fea80ade7025ebbddd25b3a6
Ryan Burnside <pixeloutlaw@gmail.com>
2022-08-04 21:32:40 -0600

    Add files via upload

---
+++ divide-thirds.scm
@@ -0,0 +1,26 @@
+(script-fu-register
+ "script-fu-divide-thirds"                        ;func name
+ "Divide into thirds using guides."               ;menu label
+ "Divide the canvas into thirds by adding guides" ;Description
+ "Ryan Burnside"                                  ;author
+ "No copyright protection"                        ;copyright
+ "August 4 2022"                                  ;date
+ ""                                               ;image type
+
+ ;; Widgets and top level parameters
+ SF-IMAGE       "Image"          0
+ SF-DRAWABLE    "Drawable"       0
+ SF-ADJUSTMENT  "Steps Across"  '(7 2 16 1 1 0 SF-SLIDER)
+ SF-ADJUSTMENT  "Steps Down"    '(7 2 16 1 1 0 SF-SLIDER))
+
+
+;; Register the main function
+(script-fu-menu-register "script-fu-divide-thirds"
+                         "<Image>/Guides/")
+
+;; Main function
+(define (script-fu-divide-thirds the-image the-drawable)
+  (script-fu-guide-new-percent 1 1 0 66.666)
+  (script-fu-guide-new-percent 1 1 0 33.333)
+  (script-fu-guide-new-percent 1 1 1 66.666)
+  (script-fu-guide-new-percent 1 1 1 33.333))
