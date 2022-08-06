747b58770f1fd2294f20f12b14424caebebc5463
Ryan Burnside <pixeloutlaw@gmail.com>
2022-08-05 20:49:56 -0600

    Update divide-thirds.scm

--- divide-thirds.scm
+++ divide-thirds.scm
@@ -20,10 +20,11 @@
 
 ;; Register the main function
 (script-fu-menu-register "script-fu-divide-grid"
-                         "<Image>/Script-Fu/")
+                         "<Image>/Script-Fu/Guides/")
+
 
 ;; Helper function
-(define (script-fu-divide-sections the-image the-drawable divisions horiz) ;0 for horiz 1 for vert
+(define (script-fu-divide-sections the-image the-drawable divisions horiz) ; 0 for horiz 1 for vert
   (let loop ((i 0))
     (when (<= i divisions)
       (script-fu-guide-new-percent the-image the-drawable horiz (* (/ i divisions) 100.0))
@@ -32,11 +33,12 @@
 
 ;; Main function
 (define (script-fu-divide-grid the-image the-drawable horizontal-divisions vertical-divisions)
+  (gimp-image-undo-group-start the-image)
   (let ((horiz 0)
         (vert 1))
     (script-fu-divide-sections the-image the-drawable horizontal-divisions horiz)
-    (script-fu-divide-sections the-image the-drawable vertical-divisions vert)))
-
+    (script-fu-divide-sections the-image the-drawable vertical-divisions vert))
+  (gimp-image-undo-group-end the-image))
 
 
 (script-fu-register
@@ -49,23 +51,18 @@
  ""                                                ;image type
 
  ;; Widgets and top level parameters
- SF-IMAGE       "Image"          0
- SF-DRAWABLE    "Drawable"       0)
+ SF-IMAGE    "Image"    0
+ SF-DRAWABLE "Drawable" 0)
 
 
 ;; Register the main function
 (script-fu-menu-register "script-fu-divide-thirds"
-                         "<Image>/Script-Fu/")
+                         "<Image>/Script-Fu/Guides/")
+
 
 ;; Main function
 (define (script-fu-divide-thirds the-image the-drawable)
-  (let ((horiz 0)
-        (vert 1))
-    (script-fu-guide-new-percent the-image the-drawable horiz 66.666)
-    (script-fu-guide-new-percent the-image the-drawable horiz 33.333)
-    (script-fu-guide-new-percent the-image the-drawable vert 66.666)
-    (script-fu-guide-new-percent the-image the-drawable vert 33.333)))
-
+  (script-fu-divide-grid the-image the-drawable 3 3))
 
 (script-fu-register
  "script-fu-frame-guides"             ;func name
@@ -77,22 +74,66 @@
  ""                                   ;image type
 
  ;; Widgets and top level parameters
- SF-IMAGE       "Image"          0
- SF-DRAWABLE    "Drawable"       0)
+ SF-IMAGE    "Image"    0
+ SF-DRAWABLE "Drawable" 0)
 
 
 ;; Register the main function
 (script-fu-menu-register "script-fu-frame-guides"
-                         "<Image>/Script-Fu/")
+                         "<Image>/Script-Fu/Guides/")
 
 ;; Main function
 (define (script-fu-frame-guides the-image the-drawable)
+  (script-fu-divide-grid the-image the-drawable 1 1))
+
+
+(script-fu-register
+ "script-fu-divide-center"    ;func name
+ "Draw guides at the center." ;menu label
+ "Draw guides at the center." ;Description
+ "Ryan Burnside"              ;author
+ "No copyright protection"    ;copyright
+ "August 4 2022"              ;date
+ ""                           ;image type
+
+ ;; Widgets and top level parameters
+ SF-IMAGE    "Image"    0
+ SF-DRAWABLE "Drawable" 0)
+
+
+;; Register the main function
+(script-fu-menu-register "script-fu-divide-center"
+                         "<Image>/Script-Fu/Guides/")
+
+;; Main function
+(define (script-fu-divide-center the-image the-drawable)
+  (script-fu-divide-grid the-image the-drawable 2 2))
+
+(script-fu-register
+ "script-fu-golden-ratio-guides" ;func name
+ "Add guides at Golden Ratio."   ;menu label
+ "Add guides at Golden Ratio."   ;Description
+ "Ryan Burnside"                 ;author
+ "No copyright protection"       ;copyright
+ "August 5 2022"                 ;date
+ ""                              ;image type
+
+ ;; Widgets and top level parameters
+ SF-IMAGE    "Image"    0
+ SF-DRAWABLE "Drawable" 0)
+
+
+;; Register the main function
+(script-fu-menu-register "script-fu-golden-ratio-guides"
+                         "<Image>/Script-Fu/Guides/")
+
+;; Main function
+(define (script-fu-golden-ratio-guides the-image the-drawable)
+  (gimp-image-undo-group-start the-image)
   (let ((horiz 0)
         (vert 1))
-    (script-fu-guide-new-percent the-image the-drawable horiz 100)
-    (script-fu-guide-new-percent the-image the-drawable horiz 0)
-    (script-fu-guide-new-percent the-image the-drawable vert 100)
-    (script-fu-guide-new-percent the-image the-drawable vert 0)))
-
-
-
+    (script-fu-guide-new-percent the-image the-drawable horiz 61.8)
+    (script-fu-guide-new-percent the-image the-drawable horiz 38.2)
+    (script-fu-guide-new-percent the-image the-drawable vert 61.8)
+    (script-fu-guide-new-percent the-image the-drawable vert 38.2))
+  (gimp-image-undo-group-end the-image))
